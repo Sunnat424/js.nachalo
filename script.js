53d8c43a26d4ee269a1df1916f9b2ce7eff47af9
Sunat <21@.ru>
2021-12-16 19:52:57 +0500

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+var vaw = 'ваш ответ'
+
+var str = prompt ('Введите имя') 
+console.log('меня зовут ' + str );
+var str = prompt ('сколько вам лет')
+console.log('меня ' + str );
+var str = +prompt ('пример1: 10+5 =')
+console.log('пример1: 10+5 = ' +(10 + 5 ) + ' ваш ответ ' + str );
+var str = +prompt ('пример2: 10-5 =')
+console.log('пример2: 10-5 = ' + (10 - 5 ) + ' ваш ответ ' + str );
+
+var str = +prompt ('пример3: 10*5 =')
+console.log('пример3: 10-5 = ' + (10 * 5 ) + ' ваш ответ ' + str );
+
+var str = +prompt ('пример4: 10/5 =')
+console.log('пример4: 10-5 = ' + (10 / 5 ) + ' ваш ответ ' + str );
+var str = +prompt ('пример4: 10%5 =')
+console.log('пример4: 10%5 = ' + (10 % 5 ) + ' ваш ответ ' + str );
